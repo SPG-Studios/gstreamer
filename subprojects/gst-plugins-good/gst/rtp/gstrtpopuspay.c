de8549f26b6df6dab1d5a479ea4d035c6fb6c41a
Mathieu Duponchelle <mathieu@centricular.com>
2026-07-21 18:40:30 +0200

    rtpopus: allow any payload type
    For multiopus on chrome, web clients will usually munge the SDP
    in order to enable hidden support. To do so they need to use distinct
    payload types and as chrome is already using up much if not all the
    dynamic space the web client will fall back to using up non-reserved,
    non-dynamic payload types.
    
    rtpopuspay2 does not constrain the payload in its template caps, it
    seems safe for the C version not to do so either.

--- subprojects/gst-plugins-good/gst/rtp/gstrtpopuspay.c
+++ subprojects/gst-plugins-good/gst/rtp/gstrtpopuspay.c
@@ -81,7 +81,6 @@ GST_STATIC_PAD_TEMPLATE ("src",
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS ("application/x-rtp, "
         "media = (string) \"audio\", "
-        "payload = (int) " GST_RTP_PAYLOAD_DYNAMIC_STRING ", "
         "clock-rate = (int) 48000, "
         "encoding-name = (string) { \"OPUS\", \"X-GST-OPUS-DRAFT-SPITTKA-00\", \"MULTIOPUS\" }")
     );
